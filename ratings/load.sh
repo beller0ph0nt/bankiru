3d56818b0d04c63a5785b475844d4b1ac1f71366
beller0ph0nt <kras_den@rambler.ru>
2016-10-09 23:36:53 +0700

    корректировка скрипта считывания таблиц

--- ratings/load.sh
+++ ratings/load.sh
@@ -5,8 +5,9 @@ function load_rating()
     local PROPERTY_ID=$1
     local DATE_1=$2
     local DATE_2=$3
-    local OUTPUT_FILE=$4
+    local OUTPUT_FILE=$4"__"$DATE_1"__"$DATE_2
 
+    local URL="http://www.banki.ru/banks/ratings/export.php"
     local POST_DATA=""
 
     POST_DATA+="PROPERTY_ID="$PROPERTY_ID"&"
@@ -19,8 +20,10 @@ function load_rating()
     POST_DATA+="IS_SHOW_GROUP=0&"
     POST_DATA+="IS_SHOW_LIABILITIES=0"
 
-    wget -o $OUTPUT_FILE --post-data $POST_DATA -c \
-         http://www.banki.ru/banks/ratings/export.php
+    echo "loading "$OUTPUT_FILE"..."
+
+    wget -q -O - --post-data $POST_DATA -c $URL | \
+    iconv -f WINDOWS-1251 -t UTF-8 > $OUTPUT_FILE
 }
 
 # Активы нетто - активы действительные (реальные) после вычета пассивов
@@ -28,15 +31,13 @@ function load_rating()
 
 NET_ASSETS_ID=10
 
-#wget --post-data 'PROPERTY_ID=10&search[type]=name&sort_param=rating&sort_order=ASC&REGION_ID=0&date1=2016-09-01&date2=2016-01-01&IS_SHOW_GROUP=0&IS_SHOW_LIABILITIES=0' \
-#     -c http://www.banki.ru/banks/ratings/export.php
-
-
 # Чистая прибыль - чистая прибыль до уплаты налогов
-# http://www.banki.ru/banks/ratings/export.php?PROPERTY_ID=30&search[type]=name&sort_param=rating&sort_order=ASC&REGION_ID=0&date1=2016-09-01&date2=2016-01-01&IS_SHOW_GROUP=0&IS_SHOW_LIABILITIES=0
 
 NET_PROFIT_ID=30
 
 
-load_rating NET_ASSETS_ID "2016-09-01" "2016-01-01" "net_assets"
-load_rating NET_PROFIT_ID "2016-09-01" "2016-01-01" "net_profit"
+load_rating $NET_ASSETS_ID "2016-01-01" "2016-09-01" "net-assets"
+load_rating $NET_ASSETS_ID "2016-09-01" "2016-01-01" "net-assets"
+
+
+#load_rating $NET_PROFIT_ID "2016-01-01" "2016-09-01" "net-profit"
