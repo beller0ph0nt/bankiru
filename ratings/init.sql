86fbb7110e8ddd89528d5418a641dd401ddc01be
beller0ph0nt <kras_den@rambler.ru>
2016-10-11 22:58:46 +0700

    корректировка скрипта загрузки таблиц

---
+++ ratings/init.sql
@@ -0,0 +1,139 @@
+create database bankiru
+
+create table rating2008
+(
+    pos         integer,
+    pos_delta   integer,
+    bank_name   char(200),
+    lic         integer primary key,
+    reg         char(200),
+    money_end   bigint,
+    money_start bigint,
+    money_delta bigint,
+    prcnt_delta real
+);
+
+create table rating2009
+(
+    pos         integer,
+    pos_delta   integer,
+    bank_name   char(200),
+    lic         integer primary key,
+    reg         char(200),
+    money_end   bigint,
+    money_start bigint,
+    money_delta bigint,
+    prcnt_delta real
+);
+
+create table rating2010 
+(
+    pos         integer,
+    pos_delta   integer,
+    bank_name   char(200),
+    lic         integer primary key,
+    reg         char(200),
+    money_end   bigint,
+    money_start bigint,
+    money_delta bigint,
+    prcnt_delta real
+);
+
+create table rating2011 (pos integer, pos_delta integer, bank_name char(200), lic integer primary key, reg char(200), money_end bigint, money_start bigint, money_delta bigint, prcnt_delta real);
+create table rating2012 (pos integer, pos_delta integer, bank_name char(200), lic integer primary key, reg char(200), money_end bigint, money_start bigint, money_delta bigint, prcnt_delta real);
+create table rating2013 (pos integer, pos_delta integer, bank_name char(200), lic integer primary key, reg char(200), money_end bigint, money_start bigint, money_delta bigint, prcnt_delta real);
+create table rating2014 (pos integer, pos_delta integer, bank_name char(200), lic integer primary key, reg char(200), money_end bigint, money_start bigint, money_delta bigint, prcnt_delta real);
+create table rating2015 (pos integer, pos_delta integer, bank_name char(200), lic integer primary key, reg char(200), money_end bigint, money_start bigint, money_delta bigint, prcnt_delta real);
+
+create table rating20142015 (pos integer, pos_delta integer, bank_name char(200), lic integer primary key, reg char(200), money_end bigint, money_start bigint, money_delta bigint, prcnt_delta real);
+create table rating20132015 (pos integer, pos_delta integer, bank_name char(200), lic integer primary key, reg char(200), money_end bigint, money_start bigint, money_delta bigint, prcnt_delta real);
+
+copy "rating2008" from '/home/vdv/banks/rating2008.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+copy "rating2009" from '/home/vdv/banks/rating2009.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+copy "rating2010" from '/home/vdv/banks/rating2010.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+copy "rating2011" from '/home/vdv/banks/rating2011.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+copy "rating2012" from '/home/vdv/banks/rating2012.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+copy "rating2013" from '/home/vdv/banks/rating2013.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+copy "rating2014" from '/home/vdv/banks/rating2014.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+copy "rating2015" from '/home/vdv/banks/rating2015.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+
+copy "rating20142015" from '/home/vdv/banks/rating20142015.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+copy "rating20132015" from '/home/vdv/banks/rating20132015.csv' with (delimiter E'\t', encoding 'windows-1251', header 1, format csv);
+
+select bank_name, lic, avg(coalesce(cast((money_start * (cast((prcnt_delta * 100) as bigint))) as bigint), 0)) as avg_mx from
+(
+ (select * from rating2008)
+  union all
+ (select * from rating2009)
+  union all
+ (select * from rating2010)
+  union all
+ (select * from rating2011)
+  union all
+ (select * from rating2012)
+  union all
+ (select * from rating2013)
+  union all
+ (select * from rating2014)
+  union all
+ (select * from rating2015)
+--  union all
+-- (select * from rating20142015)
+--  union all
+-- (select * from rating20132015)
+) as union_table
+group by bank_name, lic
+order by avg_mx desc limit 50;
+
+select bank_name, lic, avg(coalesce(cast((money_delta * (cast((prcnt_delta * 100) as bigint))) as bigint), 0)) as avg_mx from
+(
+ (select * from rating2008)
+  union all
+ (select * from rating2009)
+  union all
+ (select * from rating2010)
+  union all
+ (select * from rating2011)
+  union all
+ (select * from rating2012)
+  union all
+ (select * from rating2013)
+  union all
+ (select * from rating2014)
+  union all
+ (select * from rating2015)
+--  union all
+-- (select * from rating20142015)
+--  union all
+-- (select * from rating20132015)
+) as union_table
+group by bank_name, lic
+order by avg_mx desc limit 50;
+
+--select bank_name from
+--(
+ select bank_name, lic, avg(md) as avg_money_delta, avg(pd) as avg_prcnt_delta from
+ (
+  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating2008)
+   union all
+  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating2009)
+   union all
+  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating2010)
+   union all
+  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating2011)
+   union all
+  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating2012)
+   union all
+  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating2013)
+   union all
+  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating2014)
+   union all
+  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating2015)
+--   union all
+--  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating20142015)
+--   union all
+--  (select bank_name, lic, coalesce(money_delta, 0) as md, coalesce(prcnt_delta, 0.0) as pd from rating20132015)
+ ) as union_table
+ group by bank_name, lic
+ order by avg_prcnt_delta desc, avg_money_delta desc limit 50;
+--) as res;
--- ratings/load.sh
+++ ratings/load.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+CUR_DIR=`pwd`
+RATING_DIR=$CUR_DIR"/ratings"
+
+if [ ! -d "$RATIING_DIR" ]; then
+    mkdir $RATING_DIR
+fi
+
 function load_rating()
 {
     local PROPERTY_ID=$1
-    local DATE_1=$2
-    local DATE_2=$3
-    local OUTPUT_FILE=$4"__"$DATE_1"__"$DATE_2
+    local DATE_FROM=$2
+    local DATE_TO=$3
+    local OUTPUT_FILE=$4"__"$DATE_FROM"__"$DATE_TO
 
     local URL="http://www.banki.ru/banks/ratings/export.php"
     local POST_DATA=""
@@ -15,29 +22,98 @@ function load_rating()
     POST_DATA+="sort_param=rating&"
     POST_DATA+="sort_order=ASC&"
     POST_DATA+="REGION_ID=0&"
-    POST_DATA+="date1="$DATE_1"&"
-    POST_DATA+="date2="$DATE_2"&"
+    POST_DATA+="date1="$DATE_TO"&"
+    POST_DATA+="date2="$DATE_FROM"&"
     POST_DATA+="IS_SHOW_GROUP=0&"
     POST_DATA+="IS_SHOW_LIABILITIES=0"
 
     echo "loading "$OUTPUT_FILE"..."
 
     wget -q -O - --post-data $POST_DATA -c $URL | \
-    iconv -f WINDOWS-1251 -t UTF-8 > $OUTPUT_FILE
+    iconv -f WINDOWS-1251 -t UTF-8 | \
+    sed -e 's/\r//' > $RATING_DIR"/"$OUTPUT_FILE
 }
 
-# Активы нетто - активы действительные (реальные) после вычета пассивов
-# (долговых обязательств)
-
+# Активы нетто - активы действительные (реальные) после вычета пассивов (долговых обязательств)
 NET_ASSETS_ID=10
 
-# Чистая прибыль - чистая прибыль до уплаты налогов
+# Капитал - (собственные средства) необходим для того, чтобы в случае неожиданных финансовых
+# потерь и даже ожидаемых убытков обеспечить банку исполнение всех обязательств перед
+# кредиторами и акционерами
+#CAPITAL_ID=25
 
+# Чистая прибыль - чистая прибыль до уплаты налогов
 NET_PROFIT_ID=30
 
+# Кредитный портфель - совокупность кредитов, выданных банком.
+CREDIT_PORTFOLIO_ID=40
 
-load_rating $NET_ASSETS_ID "2016-01-01" "2016-09-01" "net-assets"
-load_rating $NET_ASSETS_ID "2016-09-01" "2016-01-01" "net-assets"
+# Вклады физических лиц - денежные средства физических лиц, размещенные на счетах в
+# коммерческих банках на определенных условиях
+DEPOSITS_ID=60
 
 
-#load_rating $NET_PROFIT_ID "2016-01-01" "2016-09-01" "net-profit"
+
+
+# 2008
+load_rating $NET_ASSETS_ID       "2008-01-01" "2008-12-01" "net-assets"
+#load_rating $CAPITAL_ID          "2008-01-01" "2008-12-01" "capital"
+load_rating $NET_PROFIT_ID       "2008-01-01" "2008-12-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2008-01-01" "2008-12-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2008-01-01" "2008-12-01" "deposits"
+
+# 2009
+load_rating $NET_ASSETS_ID       "2009-01-01" "2009-12-01" "net-assets"
+#load_rating $CAPITAL_ID          "2009-01-01" "2009-12-01" "capital"
+load_rating $NET_PROFIT_ID       "2009-01-01" "2009-12-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2009-01-01" "2009-12-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2009-01-01" "2009-12-01" "deposits"
+
+# 2010
+load_rating $NET_ASSETS_ID       "2010-01-01" "2010-12-01" "net-assets"
+#load_rating $CAPITAL_ID          "2010-01-01" "2010-12-01" "capital"
+load_rating $NET_PROFIT_ID       "2010-01-01" "2010-12-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2010-01-01" "2010-12-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2010-01-01" "2010-12-01" "deposits"
+
+# 2011
+load_rating $NET_ASSETS_ID       "2011-01-01" "2011-12-01" "net-assets"
+#load_rating $CAPITAL_ID          "2011-01-01" "2011-12-01" "capital"
+load_rating $NET_PROFIT_ID       "2011-01-01" "2011-12-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2011-01-01" "2011-12-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2011-01-01" "2011-12-01" "deposits"
+
+# 2012
+load_rating $NET_ASSETS_ID       "2012-01-01" "2012-12-01" "net-assets"
+#load_rating $CAPITAL_ID          "2012-01-01" "2012-12-01" "capital"
+load_rating $NET_PROFIT_ID       "2012-01-01" "2012-12-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2012-01-01" "2012-12-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2012-01-01" "2012-12-01" "deposits"
+
+# 2013
+load_rating $NET_ASSETS_ID       "2013-01-01" "2013-12-01" "net-assets"
+#load_rating $CAPITAL_ID          "2013-01-01" "2013-12-01" "capital"
+load_rating $NET_PROFIT_ID       "2013-01-01" "2013-12-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2013-01-01" "2013-12-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2013-01-01" "2013-12-01" "deposits"
+
+# 2014
+load_rating $NET_ASSETS_ID       "2014-01-01" "2014-12-01" "net-assets"
+#load_rating $CAPITAL_ID          "2014-01-01" "2014-12-01" "capital"
+load_rating $NET_PROFIT_ID       "2014-01-01" "2014-12-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2014-01-01" "2014-12-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2014-01-01" "2014-12-01" "deposits"
+
+# 2015
+load_rating $NET_ASSETS_ID       "2015-01-01" "2015-12-01" "net-assets"
+#load_rating $CAPITAL_ID          "2015-01-01" "2015-12-01" "capital"
+load_rating $NET_PROFIT_ID       "2015-01-01" "2015-12-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2015-01-01" "2015-12-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2015-01-01" "2015-12-01" "deposits"
+
+#2016
+load_rating $NET_ASSETS_ID       "2016-01-01" "2016-09-01" "net-assets"
+#load_rating $CAPITAL_ID          "2016-01-01" "2016-09-01" "capital"
+load_rating $NET_PROFIT_ID       "2016-01-01" "2016-09-01" "net-profit"
+load_rating $CREDIT_PORTFOLIO_ID "2016-01-01" "2016-09-01" "credit-portfolio"
+load_rating $DEPOSITS_ID         "2016-01-01" "2016-09-01" "deposits"
